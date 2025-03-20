814a5078627180fe5bb95ef16ee6564922b9a52e
Saif Khayoon <88499318+Khayoon@users.noreply.github.com>
2025-03-20 12:09:38 -0400

    Create urlsplitter.py

---
+++ urlsplitter.py
@@ -0,0 +1,13 @@
+def count_strings_between_plus(input_string):
+    count = 0
+    if '+' in input_string:
+        parts = input_string.split('+')
+        count = len(parts) - 1
+    return count
+
+#  input
+string = " "
+
+count = count_strings_between_pluses(string)
+
+print(f"'{string}' has {count1} strings.")
